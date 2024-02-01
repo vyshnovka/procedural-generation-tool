6ec955d877c30c11fff674429e169350d78d5d99
Vykula <viktoria.zheneral@gmail.com>
2024-02-01 21:37:44 +0200

    removed test code
    updated the UI instead

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -17,20 +17,6 @@ public class UIManager : MonoBehaviour
     {
         var root = document.rootVisualElement;
 
-        var algorithmDropdown = root.Q<DropdownField>("AlgorithmDropdown");
-        algorithmDropdown.choices = Enum.GetValues(typeof(AlgorithmType))
-                                        .Cast<AlgorithmType>()
-                                        .Select(v => v.ToString())
-                                        .ToList();
-        algorithmDropdown.value = algorithmDropdown.choices[0];
-
-        var sizeDropdown = root.Q<DropdownField>("SizeDropdown");
-        sizeDropdown.choices = Enum.GetValues(typeof(Size))
-                                   .Cast<Size>()
-                                   .Select(v => v.ToString())
-                                   .ToList();
-        sizeDropdown.value = sizeDropdown.choices[0];
-
         root.Q<Button>("Generate").clicked += () => terrainGenerator.DisplayResult();
     }
 }
